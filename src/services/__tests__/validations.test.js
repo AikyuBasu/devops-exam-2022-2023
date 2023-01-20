8bf02b4463fca58d88c780b5514e4a325bded1a5
AikyuBasu <boltryk.alicia@gmail.com>
2023-01-20 10:22:20 +0100

    test: add test01: character requirment

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -1,4 +1,4 @@
-const { isEmpty } = require("../validations");
+const { isEmpty, isLongEnough } = require("../validations");
 
 describe("validations tests suites - isEmpty", () => {
     test("should return true as the label is undefined", () => {
@@ -18,3 +18,21 @@ describe("validations tests suites - isEmpty", () => {
 });
 
 // TODO: Create tests suite for validation function
+
+describe("validations tests suites - gamertag is at least 8char long", () => {
+    test("should return true as the label is 8 char long", () => {
+        const result = isLongEnough("gamertag");
+        expect(result).toBe(true);
+    });
+
+    test("should return true as the label is more than 8 char long", () => {
+        const result = isLongEnough("gamertag0123");
+        expect(result).toBe(true);
+    });
+
+    test("should return true as the label 8 char long", () => {
+        const result = isLongEnough("gamer1");
+        expect(result).toBe(false);
+    });
+
+});
--- src/services/validations.js
+++ src/services/validations.js
@@ -2,4 +2,8 @@
 
 const isEmpty = (label) => !label || label.length === 0;
 
+const isLongEnough = (label) => label.length >= 8;
+
 exports.isEmpty = isEmpty;
+exports.isLongEnough = isLongEnough;
+
